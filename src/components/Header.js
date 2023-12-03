c4e22ade81c1840178588b33e17708f76e15c7cc
taku-chimanaz <tkchimanaz@gmail.com>
2023-12-04 00:58:41 +0200

    implemented the Header

---
+++ src/components/Header.js
@@ -0,0 +1,85 @@
+import React, { useRef, useState } from 'react';
+import './../css/Header.css';
+import CloseIcon from './../images/icon-close.svg'
+import MenuIcon from './../images/icon-menu.svg';
+import CartIcon from './../images/icon-cart.svg';
+import UserAvatar from './../images/image-avatar.png';
+import { Cart } from './Cart';
+
+
+export const Header = () => {
+
+    const [displayCart, setDisplayCart] = useState(false);
+    const navContainer = useRef();
+
+    const toggleVerticalNav = () => {
+        navContainer.current.classList.toggle('displayNav');
+    }
+
+    return (
+
+        <header>
+
+            {/* burger */}
+
+            <section className="header__left-side">
+
+                <button className="menu-btn" onClick={toggleVerticalNav}>
+                    <img src={MenuIcon} alt="Menu Icon" />
+                </button>
+
+                {/* logo */}
+
+                <div className="logo-container">
+                    <h1 className="logo">
+                        sneakers
+                    </h1>
+                </div>
+
+                {/*  navigation*/}
+
+                <div className="nav-container" ref={navContainer}>
+
+
+
+                    <nav>
+                        <button className="close-btn" onClick={toggleVerticalNav}>
+                            <img src={CloseIcon} alt="Close Icon" />
+                        </button>
+                        <a href="#">collections</a>
+                        <a href="#">men</a>
+                        <a href="#">women</a>
+                        <a href="#">about</a>
+                        <a href="#">contact</a>
+
+                    </nav>
+                </div>
+
+            </section>
+
+            {/* Cart-profile avatar */}
+
+            <section className="header__right_side">
+
+                <button className="cart-btn" onClick={() => setDisplayCart(!displayCart)}>
+                    <img src={CartIcon} alt="" />
+                    <p className="cart-items-counter">
+                        1
+                    </p>
+                </button>
+
+                {
+                    displayCart &&
+                    <Cart />
+                }
+
+                <div className="user-profile-avatar-container">
+                    <img src={UserAvatar} alt="User Avatar" />
+                </div>
+
+            </section>
+
+
+        </header>
+    )
+}
